4bda422262282c28a6c40039ed0fa539144bcfde
Nathan Firmo <nathanssfirmo@gmail.com>
2021-05-05 16:49:40 -0300

    Formatando o código

--- src/controllers/users/listar.js
+++ src/controllers/users/listar.js
@@ -4,16 +4,29 @@ const decrypt = require('../functions').decrypt
 require('dotenv/config')
 
 module.exports = {
-    store(req, res){
+    store(req, res) {
         mysql.getConnection((err, conn) => {
             console.log("OKOK")
-            if(err){ return res.status(500).json({error:err})}
-            conn.query('select * from users' , (error, response) => {
+            if (err) {
+                return res.status(500).json(
+                    {
+                        error: err
+                    }
+                )
+            }
+            conn.query('select * from users', (error, response) => {
                 conn.release()
-                if(error){return res.status(500).json({
-                    error:error.sqlMessage})}
-                if(response.length === 0){res.status(404).json({message: "Não encontrado"})}
-                return res.status(200).json({ 
+                if (error) {
+                    return res.status(500).json({
+                        error: error.sqlMessage
+                    })
+                }
+                if (response.length === 0) {
+                    res.status(404).json({
+                        message: "Não encontrado"
+                    })
+                }
+                return res.status(200).json({
                     message: "Usuários no banco de dados",
                     response
                 })
--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -7,40 +7,66 @@ const decrypt = require('../functions').decrypt
 require('dotenv/config')
 
 module.exports = {
-    store(req,res){
+    store(req, res) {
         mysql.getConnection((err, conn) => {
-            if(err){ return res.status(500).json({error:err})}
+            if (err) {
+                return res.status(500).json({
+                    error: err
+                })
+            }
             const email = encrypt(req.body.email)
-            conn.query(`select * from ${process.env.TABELA1} where email = ?`, [email], (error, response)=>{
-                if(error){return res.status(500).json({error:error.sqlMessage})}
-                if(response.length < 1){
-                    return res.status(401).json({message: "Falha na autenticação"})
-                } 
-                bcrypt.compare(req.body.password, response[0].password, (errBcrypt, results)=>{
-                    if(errBcrypt){
-                        return res.status(401).json({message: "Falha na autenticação"})
-                    }
-                    if(results){
-                        if(!response[0].confirmed){
-                            return res.status(401).json({message: "Email não confirmado."})
-                        }
-                        const token = jwt.sign({
-                            id_usuario: response[0].id_user
-                        }, process.env.JWT_SECRET, 
-                        {
-                            expiresIn: "25d"
+            conn.query(`select * from ${process.env.TABELA1} where email = ?`, [email], (error, response) => {
+                if (error) {
+                    return res.status(500).json({
+                        error: error.sqlMessage
+                    })
+                }
+                if (response.length < 1) {
+                    return res.status(401).json({
+                        message: "Falha na autenticação"
+                    })
+                }
+                bcrypt.compare(req.body.password, response[0].password, (errBcrypt, results) => {
+                    if (errBcrypt) {
+                        return res.status(401).json({
+                            message: "Falha na autenticação"
                         })
-                        conn.query(`update ${process.env.TABELA1} set token = ? where id_user = ?`, [token, response[0].id_user], (err, sucess)=>{
+                    }
+
+                    if (results) {
+                        if (!response[0].confirmed) {
+                            return res.status(401).json({
+                                message: "Email não confirmado."
+                            })
+                        }
+
+                        const token = jwt.sign(
+                            {
+                                id_usuario: response[0].id_user
+                            },
+                            process.env.JWT_SECRET,
+                            {
+                                expiresIn: "25d"
+                            })
+
+                        conn.query(`update ${process.env.TABELA1} set token = ? where id_user = ?`, [token, response[0].id_user], (err, sucess) => {
+                            
                             conn.release()
-                            if(err){return res.status(500).json({error:err.sqlMessage})}
+
+                            if (err) {
+                                return res.status(500).json({
+                                    error: err.sqlMessage
+                                }
+                                )
+                            }
                             return res.status(200).send({
-                                message:"Logado com sucesso",
+                                message: "Logado com sucesso",
                                 token: token
                             })
                         })
                     }
-                    else{
-                        return res.status(401).json({message: "Falha na autenticação"})
+                    else {
+                        return res.status(401).json({ message: "Falha na autenticação" })
                     }
                 })
             })
