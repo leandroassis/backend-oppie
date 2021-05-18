83fcb46682942ed1d9338cb71abd180f9734b51f
Nathan Firmo <nathanssfirmo@gmail.com>
2021-05-18 11:30:44 -0300

    :tada: updating project pattern

--- src/controllers/functions.js
+++ src/controllers/functions.js
@@ -6,20 +6,20 @@ require('dotenv/config')
 
 module.exports = {
     // função para encriptar dados sensiveis
-    encrypt(value){
+    encrypt(value) {
         const cypher = crypto.createCipheriv(process.env.CRYPTO_ALGORITHM, process.env.CRYPTO_PASSWORD, process.env.CRYPTO_IV);
         var encrypted = cypher.update(value, 'utf-8', process.env.CRYPTO_BASE)
         return encrypted += cypher.final(process.env.CRYPTO_BASE)
     },
     // função para descriptar dados sensiveis
-    decrypt(value){
+    decrypt(value) {
         const decypher = crypto.createDecipheriv(process.env.CRYPTO_ALGORITHM, process.env.CRYPTO_PASSWORD, process.env.CRYPTO_IV)
         var decrypted = decypher.update(value, process.env.CRYPTO_BASE, 'utf-8')
         decrypted += decypher.final('utf-8')
         return decrypted
     },
     // função que faz o envio do email de verificação
-    confirmEmail(email, emailToken){
+    confirmEmail(email, emailToken) {
         const url = `http://localhost:3333/confirmation/email/?token=${emailToken}`
 
         const transporter = nodemailer.createTransport({
@@ -42,16 +42,16 @@ module.exports = {
             <a href="${url}">${url}</a>
             <p>Não adianta clicar, é só um teste :p</p>
             `
-        }).then(info =>{
+        }).then(info => {
             console.log(info)
-        }).catch(email_error =>{
+        }).catch(email_error => {
             console.log(email_error)
         })
     },
-    confirmPhone(phone){
+    confirmPhone(phone) {
         const code = crypto.randomBytes(3).toString('hex').toUpperCase();
         const apiKey = process.env.SMS_API_KEY
-        
+
         var textMessageService = new TextMessageService(apiKey);
         textMessageService.send(
             `Teste APi Código de Verificação ${code}`,   // Content: Conteudo da mensagem a ser enviada.
