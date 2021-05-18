2087d4114d5f326b497522c510187813bdfaa49b
Nathan Firmo <nathanssfirmo@gmail.com>
2021-05-18 11:30:44 -0300

    :tada: updating project pattern

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,12 @@ To do Rota de confirmação de telefone
 To do Cadastro/Login com redes sociais
 */
 
-const cadastrarUser = require('./controllers/users/cadastro')
-const logarUser = require('./controllers/users/login')
-const listarUsers = require('./controllers/users/listar')
-const confirmEmail = require('./controllers/confirmation/confirmEmail')
-const requestConfirmPhone = require('./controllers/confirmation/requestConfirmPhone')
-const ConfirmPhone = require('./controllers/confirmation/ConfirmCode')
+const cadastrarUser = require('./controllers/users/Register')
+const logarUser = require('./controllers/users/Login')
+const listarUsers = require('./controllers/users/ListUsers')
+const confirmEmail = require('./controllers/confirmation/ConfirmEmail')
+const requestconfirmPhone = require('./controllers/confirmation/RequestConfirmPhone')
+const confirmPhone = require('./controllers/confirmation/ConfirmCode')
 
 
 routes.post('/users/cadastro', cadastrarUser.store)
@@ -19,9 +19,7 @@ routes.post('/users/login', logarUser.store)
 routes.get('/', listarUsers.store)
 
 routes.get('/confirmation/email', confirmEmail.store)
-routes.get('/confirmation/request_code', requestConfirmPhone.store)
-routes.get('/confirmation/confirm_code', ConfirmPhone.store)
-
-
+routes.get('/confirmation/request_code', requestconfirmPhone.store)
+routes.get('/confirmation/confirm_code', confirmPhone.store)
 
 module.exports = routes;
