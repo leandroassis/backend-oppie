82265420903730b68f317312021937982b72fc9c
Nathan Firmo <nathanssfirmo@gmail.com>
2021-06-02 15:43:21 -0300

    corrigindo nodemailer e token

--- src/controllers/confirmation/ConfirmEmail.js
+++ src/controllers/confirmation/ConfirmEmail.js
@@ -1,6 +1,7 @@
 const mysql = require('../../mysql').pool
 require('dotenv/config')
 
+
 module.exports = {
     store(req, res) {
         mysql.getConnection((err, conn) => {
@@ -11,19 +12,35 @@ module.exports = {
                 })
             }
 
-            conn.query(`update ${process.env.TABELA1} set confirmed = ? where token = ?`, [true, req.query.token], (error, response) => {
 
-                conn.release()
+            conn.query(`SELECT * FROM ${process.env.TABELA1} WHERE token = ?`, [req.query.token] , (error, responseQuery) => {
                 if (error) {
                     return res.status(500).json({
                         error: error.sqlMessage
                     })
                 }
+                if (JSON.parse(JSON.stringify(responseQuery))[0] != undefined) {
+                    conn.query(`update ${process.env.TABELA1} set confirmed = ? where token = ?`, [true, req.query.token], (error, response) => {
 
-                return res.status(200).json({
-                    message: "Confirmado com sucesso",
-                    token: req.query.token
-                })
+                        conn.release()
+                        if (error) {
+                            return res.status(500).json({
+                                error: error.sqlMessage
+                            })
+                        }
+
+                        return res.status(200).json({
+                            message: "Confirmado com sucesso",
+                            token: req.query.token
+                        })
+                    })
+                }
+                else {
+                    return res.status(400).json({
+                        message: "O tokem é inválido",
+                        token: req.query.token
+                    })
+                }
             })
         })
     }
